70ca533079343dd7557ee73a243ed86ab5e5ea84
Nikolaos Bellias <nikolaos.bellias@zuehlke.com>
2023-12-02 12:37:09 +0100

    Updates Universities

--- src/Universities/Universities.unit.test.ts
+++ src/Universities/Universities.unit.test.ts
@@ -1,20 +1,36 @@
 import fetchUniversities from "./Universities";
 
-jest.mock('../Shared/DTOs/UniversityDto');
-jest.mock('../Shared/DAL/UniversitiesDal');
 
-describe('fetchUniversities function', () => {
-  it('should expose a function', () => {
-		expect(fetchUniversities).toBeDefined();
-	});
-  
-  it('fetchUniversities should return something', async () => {
-    const retValue = await fetchUniversities('Greece');
-    expect(retValue).toReturn;
+describe('universities', () => {
+  describe('fetchUniversities function', () => {
+    it('should expose a function', () => {
+      expect(fetchUniversities).toBeDefined();
+    });
+
+    it('fetchUniversities should return something', async () => {
+      const retValue = await fetchUniversities('Greece');
+      expect(retValue).toReturn;
+    });
+
+    it('fetchUniversities should be truthy', async () => {
+      const retValue = await fetchUniversities('Greece');
+      expect(retValue).toBeTruthy;
+    });
   });
 
-  it('fetchUniversities should be truthy', async () => {
-    const retValue = await fetchUniversities('Greece');
-    expect(retValue).toBeTruthy;
+  describe('fetchUniversities from mocked Universities', async () => {
+    jest.mock('../Shared/DTOs/UniversityDto');
+  jest.mock('../Shared/DAL/UniversitiesDal');
+    jest.mock('./Universities');
+    test('fetchUniversities should return something', async () => {
+      const retValue = await fetchUniversities('Greece');
+      expect(retValue).toReturn;
+    });
+
+    test('fetchUniversities should be truthy', async () => {
+      const retValue = await fetchUniversities('Greece');
+      expect(retValue).toBeTruthy;
+    });
   });
 });
+
